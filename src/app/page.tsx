f7c3f05dcc0fb54e31d87c2a5e351b979245ea21
Dave Mascia <753866+dmascia@users.noreply.github.com>
2025-02-06 15:28:11 -0500

    readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,76 @@
-import Link from "next/link";
+"use client"
 
 import styles from "./index.module.css";
+import { useCallback, useState} from "react";
+import {faker} from "@faker-js/faker/locale/en";
+
+type User = {
+    id: string;
+    username: string;
+    email: string;
+    location: string;
+    avatar: string;
+    registeredAt: string;
+}
+
+const ROW_SIZE = 20;
 
 export default function Home() {
+    const [users] = useState<User[]>(() => {
+       return Array<User[]>(1000).fill([]).map((_: User[]): User => {
+           return {
+               id: faker.string.uuid(),
+               username: faker.internet.username(),
+               email: faker.internet.email(),
+               avatar: faker.image.avatar(),
+               location: faker.location.state(),
+               registeredAt: String(faker.date.past()),
+           };
+       });
+    });
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [filteredUsers, setFilteredUsers] = useState<User[]>(users.slice(0, currentPage * ROW_SIZE));
+
+    const debounce = (func: (...args: unknown[]) => void, wait: number) => {
+        let timeoutId: string | number | NodeJS.Timeout | undefined;
+        return (...args: unknown[]) => {
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+
+            timeoutId = setTimeout(() => {
+                func(...args)
+            }, wait);
+        }
+    }
+
+    const onScroll = useCallback(() => {
+        setCurrentPage((currentPage) => currentPage + 1);
+        setFilteredUsers(users.slice(0, currentPage * ROW_SIZE));
+    }, [currentPage, users]);
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <h1 className={styles.title}>
-          Create <span className={styles.pinkSpan}>T3</span> App
+          Infinite <span className={styles.pinkSpan}>Scroll</span> App
         </h1>
-        <div className={styles.cardRow}>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>First Steps →</h3>
-            <div className={styles.cardText}>
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>Documentation →</h3>
-            <div className={styles.cardText}>
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+          <div className={styles.scrollContainer} onScroll={debounce(onScroll, 500)}>
+              <table>
+                  <tbody>
+                      {filteredUsers.map((user: User) => {
+                          return (<tr key={user.id}>
+                              <td>{user.username}</td>
+                              <td>{user.email}</td>
+                              <td>{user.location}</td>
+                              <td>{user.registeredAt}</td>
+                          </tr>)
+                      })}
+                  </tbody>
+              </table>
+          </div>
+
+         <span className={styles.pinkSpan}>  Displaying: {currentPage * ROW_SIZE} / {users.length} </span>
       </div>
     </main>
   );
